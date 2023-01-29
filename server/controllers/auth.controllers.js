810a9f912029de6523b02827e59d26049c3ab7eb
Naim Chaya <naimchaya.dev@gmail.com>
2023-01-29 11:11:41 -0300

    Server v6

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -4,30 +4,40 @@ import User from "../models/User.js";
 import Role from "../models/Role.js";
 
 export const signUp = async (req, res) => {
-  const { username, email, password, roles } = req.body;
+  try {
+    const { username, email, password, roles } = req.body;
 
-  const users = await User.find({ email });
+    const foundUsername = await User.find({ username });
+    if (!(foundUsername.length === 0)) return res.json({ msg: "Username alredy exist" });
 
-  const newUser = await new User({
-    username,
-    email,
-    password: await User.hashPassword(password),
-  });
+    const foundEmail = await User.find({ email });
+    if (!(foundEmail.length === 0)) return res.json({ msg: "Email alredy exist" });
 
-  if (roles) {
-    const foundRoles = await Role.find({ name: { $in: roles } });
-    newUser.roles = foundRoles.map((role) => role._id);
-  } else {
-    const role = await Role.find({ name: "user" });
-    newUser.role = [role._id];
+    const newUser = await new User({
+      username,
+      email,
+      password: await User.hashPassword(password),
+    });
+
+    if (roles) {
+      const foundRoles = await Role.find({ name: { $in: roles } });
+      newUser.roles = foundRoles.map((role) => role._id);
+    } else {
+      const role = await Role.find({ name: "user" });
+      role[0]._id;
+      newUser.roles = [role[0]._id];
+    }
+
+    const savedUser = await newUser.save();
+    const token = jwt.sign({ id: savedUser._id }, SECRET_JWT, {
+      expiresIn: 24 * 60 * 60,
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Fallo en la solicitud" });
   }
-
-  const savedUser = await newUser.save();
-  const token = jwt.sign({ id: savedUser._id }, SECRET_JWT, {
-    expiresIn: 24 * 60 * 60,
-  });
-
-  res.json({ token });
 };
 
 export const signIn = async (req, res) => {
--- server/models/User.js
+++ server/models/User.js
@@ -6,12 +6,10 @@ const userSchema = new Schema(
     username: {
       type: String,
       required: true,
-      unique: true,
     },
     email: {
       type: String,
       required: true,
-      unique: true,
     },
     password: {
       type: String,
